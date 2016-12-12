f50e82b69c34e0d50ecc0ca2031b53e966a3e186
dengc <deng.chao@163.com>
2016-12-12 23:40:21 +0800

    接收解析合并

--- src/main/java/com/acorcs/wni/rabbit/ReceiveListener.java
+++ src/main/java/com/acorcs/wni/rabbit/ReceiveListener.java
@@ -1,5 +1,6 @@
 package com.acorcs.wni.rabbit;
 
+import com.acorcs.wni.WniJsonPersistenceManager;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
@@ -23,23 +25,12 @@ import java.util.Date;
 @Service
 public class ReceiveListener {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
-    private static GsonBuilder gsonBuilder = new GsonBuilder();
-    private static Gson gson = gsonBuilder.create();
-    private static JsonParser jsonParser = new JsonParser();
+    @Autowired
+    private WniJsonPersistenceManager jsonPersistenceManager;
     @RabbitListener(queues = "${wni.rabbitmq.queue}")
     @RabbitHandler
     public void process(@Payload Message payload) throws ParseException {
         String context = new String(payload.getBody());
-//        logger.info(context);
-        JsonObject jsonObject = gson.fromJson(context,JsonObject.class);
-//        JsonObject jsonObject = jsonParser.parse(context).getAsJsonObject();
-        JsonObject header = jsonObject.getAsJsonObject("header");
-        JsonObject body = jsonObject.getAsJsonObject("body");
-        String type = jsonObject.get("type").getAsString();
-        String elem = jsonObject.get("elem").getAsString();
-        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
-        Date updated = dateFormat.parse(jsonObject.get("updated").getAsString());
-//        Date updated = new Date(jsonObject.get("updated").getAsInt() * 1000L);
-        System.out.println(body);
+        jsonPersistenceManager.transformAndSave(context);
     }
 }
