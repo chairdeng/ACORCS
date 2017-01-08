2c15a719e11dfb0bee41edd7b52cf1f289197a2b
dengc <deng.chao@163.com>
2017-01-08 23:36:30 +0800

    增加ehcache缓存

---
+++ src/main/java/com/acorcs/wni/web/serializer/GeometryDeserializer.java
@@ -0,0 +1,30 @@
+package com.acorcs.wni.web.serializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.vividsolutions.jts.geom.Geometry;
+import org.geotools.geojson.geom.GeometryJSON;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+/**
+ * Created by dengc on 2017/1/8.
+ */
+public class GeometryDeserializer extends JsonDeserializer<Geometry> {
+    @Override
+    public Geometry deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        JsonToken currentToken = jsonParser.getCurrentToken();
+        String json = jsonParser.getText();
+        GeometryJSON geoJson = new GeometryJSON();
+        Reader reader = new StringReader(json);
+        Geometry geometry = geoJson.read(reader);
+        geometry.setSRID(4326);
+        jsonParser.nextToken();
+        return geometry;
+    }
+}
